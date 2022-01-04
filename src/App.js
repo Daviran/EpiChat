579a42f4b71f9492ea4964807872b34d9d7d6041
daviran <dbugnon@outlook.fr>
2022-01-04 12:48:44 +0100

    idea: chat?

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from './components/head.js';
 import Foot from './components/foot.js';
 import TheNav from './components/theNav'
-
+import Chat from './components/chat'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <Head/> 
-      
+      <TheNav/>
       </header>
 
       <main>
-      <TheNav/>
+      <Chat></Chat>
       </main>
       <footer className='App-footer'> 
 
